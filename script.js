186c81f135d65fa753f6abec6e27c09fd941b6d7
sakinarahimova <sakinarahimova18@gmail.com>
2025-08-21 00:43:14 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// let ourElement = document.querySelector("div");
+
+//     // Get real rendered size
+//     console.log("offsetWidth:", ourElement.offsetWidth);
+//     console.log("offsetHeight:", ourElement.offsetHeight);
+
+//     console.log("getBoundingClientRect:", ourElement.getBoundingClientRect());
+
+
